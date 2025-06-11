097533da7a61cdf19d14ac8dca59e24396299fdd
EpicPantalones <ethansorensen01@gmail.com>
2025-06-11 15:51:42 -0700

    I didn't stage anything lol

--- README.md
+++ README.md
@@ -4,29 +4,98 @@ To use this, you first have to have an active [OpenALPR](https://cloud.openalpr.
 
 ## Setting up the Camera
 
+This tutorial uses a Raspberry Pi 5 and a PiCam V3. You can probably use the included script to run almost any camera with a little tweaking, at least on the Raspberry Pi.
 
-## Setting up the Rekor Server
+To get the camera running, clone this repo's `lite` branch onto a Raspberry Pi:
 
+```sh
+git clone git@github.com:NET-BYU/alpr.git
+```
+
+then, just navigate to the `camera` directory and run the included `install.sh` script.
+
+```sh
+cd alpr/camera
+chmod +x install.sh
+./install.sh
+```
+
+Your Raspberry Pi now has a service file named `camera.service` running `ipcam.py` and streaming over HTTP on `0.0.0.0` (your local IP address). Anytime you turn on the Raspberry Pi, this service will start automatically. Test this by going to `http://<IP-ADDR>:8080`.
+
+You can configure the video stream by editing `ipcam.py` and it's global variables:
+
+```py
+# Output Configuration         
+WIDTH = 2560   
+HEIGHT = 1440
+FRAMERATE = 30
+PORT = 8080
+```
+
+## Setting up the Rekor Server (Windows)
+
+Once you have at least one camera up and running, set up your Rekor Scout Agent on your Windows machine. Go to Rekor's page (or [this link](https://deb.openalpr.com/windows-agent/openalpr-agent-latest.exe)) and download the Rekor Agent. Run the installer. Along the way to opening the agent, it will ask you to sign in and verify your account. Once you have installed and signed it, it should land on this program:
+
+[program](media/program_empty.png)
+
+The program is very simple to run. First, add the camera you set up in the previous step. Click Add camera, select `IP Camera (Manual)` as the type, and then enter `http://<IP-ADDR>:<PORT>`, where `<IP-ADDR>` is the configured IP address of the camera, and `<PORT>` is the whatever port you setup (default is 8080)
+
+[program](media/program_camsetup.png)
+
+Then, click `test` to make sure your camera feed is working. You can skip the credentials it asks for (unless you have a custom camera setup).
+
+[program](media/program_camtest.png)
+
+In this example, I am actually setting it up to view our 3D Printer's HTTP camera. Once you can see the feed, click `Save Camera`, name the camera, and it will show up on your dashboard.
+
+That's it! Any license plates it sees will automatically be uploaded to your server on OpenALPR's website. You can view the metrics there.
 
 ## Running your own local dashboard
 
-Once you have subscribed to OA's Rekor Scout, follow their instructions to download the client on Windows.
+This repository also contains an `alpr_integrated_server.py` which runs a local flask dashboard that mimics the OpenALPR dashboard, but runs locally without internet. To run it, first start the flask server, then point the Rekor Scout Agent to the server instead of the OpenALPR server.
 
-To connect the webserver to Rekor Scout:
+To use the server, your Windows computer must have python installed (I used python 3.10, but any modern Python should work). Once you have that installed, run
 
-Open Rekor Scount:
+```sh
+python3.10 -m venv .venv
+.\.venv\Scripts\activate
+python3.10 -m pip install flask
+```
 
-![program](media/program.png)
+to start the server, simply run
 
-Click on the top left, **Configure >> Data Destinations**:
+```sh
+python3.10 .\alpr_integrated_server.py
+```
 
-![program](media/data_dest.png)
+You now have an openALPR mimic server that stores all its own local data. the server generates three different files:
 
-Click on Other HTTP Server:
+- `alpr_raw_data.jsonl`: contains all the JSON reported by the server, including heartbeat messages.
+- `alpr_parsed_data.jsonl`: contains just the useful license plate data. Configurable if you edit the integrated server script.
+- `event.log`: a condensed event log of what the server reports.
 
-![program](media/server_dialogue.png)
+It also stores all the images received in the `plates` folder. The server webpage looks like this:
 
-Enter `http://localhost:5000/alpr` as the server URL:
+[webpage](media/webpage.png)
 
-![program](media/url_dest.png)
+_the error fetching data was because I wasn't running the server when I took the picture._
+
+To connect Rekor Scout to your local webserver:
+
+1. run your server.
+2. Open Rekor Scount:
+
+    ![program](media/program_empty.png)
+
+3. Click on the top left, **Configure >> Data Destinations**:
+
+    ![program](media/program_datadest.png)
+
+4. Click on Other HTTP Server:
+
+    ![program](media/program_datadest_popup.png)
+
+5. Enter `http://localhost:5000/alpr` as the server URL:
+
+    ![program](media/program_urldest_popup.png)
 
---
+++ camera/ipcam.py
@@ -0,0 +1,102 @@
+import subprocess
+import threading                                           
+import http.server
+import socketserver 
+import signal 
+import sys                
+import queue                
+
+# Output Configuration         
+WIDTH = 2560   
+HEIGHT = 1440
+FRAMERATE = 30
+PORT = 8080
+
+libcamera_cmd = [
+    "libcamera-vid",
+    "-t", "0",       
+    "--width", str(WIDTH),     
+    "--height", str(HEIGHT),           
+    "--framerate", str(FRAMERATE),
+    "--codec", "yuv420",
+    "--inline",   
+    "-o", "-"   
+]      
+                                               
+ffmpeg_cmd = [                                             
+    "ffmpeg",                                                                                  
+    "-f", "rawvideo",                                      
+    "-pixel_format", "yuv420p",      
+    "-video_size", f"{WIDTH}x{HEIGHT}",                    
+    "-framerate", str(FRAMERATE),
+    "-i", "-",                                                                                                                                                                                
+    "-f", "mjpeg",
+    "-q:v", "5",
+    "-"                                      
+]                    
+                                               
+libcamera_proc = subprocess.Popen(libcamera_cmd, stdout=subprocess.PIPE)                                               
+                                               
+frame_queue = queue.Queue(maxsize=10)       
+                                               
+def ffmpeg_reader():                                                                           
+    ffmpeg_proc = subprocess.Popen(ffmpeg_cmd, stdin=libcamera_proc.stdout, stdout=subprocess.PIPE, bufsize=0)                                                                                                                                
+    buffer = b""                       
+    while True:     
+        chunk = ffmpeg_proc.stdout.read(4096)
+        if not chunk:                                                                          
+            break                                                                              
+        buffer += chunk           
+        while True:     
+            start = buffer.find(b'\xff\xd8')
+            end = buffer.find(b'\xff\xd9')
+            if start != -1 and end != -1 and end > start:                                                              
+                frame = buffer[start:end+2]                                                    
+                buffer = buffer[end+2:]
+                try:                                       
+                    if frame_queue.full():                                                     
+                        _ = frame_queue.get_nowait()                                                                   
+                    frame_queue.put_nowait(frame)                                                                      
+                except queue.Full: 
+                    pass                                                                       
+            else:             
+                break
+                                               
+ffmpeg_thread = threading.Thread(target=ffmpeg_reader, daemon=True)                                                    
+ffmpeg_thread.start()                                                                          
+                                                                                               
+class MJPEGHandler(http.server.BaseHTTPRequestHandler):                
+    def do_GET(self):                 
+        if self.path == '/':               
+            self.send_response(200)                                                            
+            self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=frame')                              
+            self.end_headers()
+            try:                
+                while True:                                
+                    frame = frame_queue.get()
+                    self.wfile.write(b'--frame\r\n')                        
+                    self.send_header('Content-Type', 'image/jpeg')                                                     
+                    self.send_header('Content-Length', str(len(frame)))                                                
+                    self.end_headers()                                                         
+                    self.wfile.write(frame)
+            except (BrokenPipeError, ConnectionResetError):                                                            
+                pass                                       
+        else:                                              
+            self.send_error(404)                           
+
+def start_server():                                        
+    with socketserver.ThreadingTCPServer(("", PORT), MJPEGHandler) as httpd:                                           
+        httpd.serve_forever()                              
+
+server_thread = threading.Thread(target=start_server, daemon=True)                                                     
+server_thread.start()                                      
+
+def cleanup(signum, frame):                                
+    libcamera_proc.terminate()                             
+    libcamera_proc.wait()                                  
+    sys.exit(0)                                            
+
+signal.signal(signal.SIGINT, cleanup)                      
+signal.signal(signal.SIGTERM, cleanup)                     
+
+signal.pause()  
